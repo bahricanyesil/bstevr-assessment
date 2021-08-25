d61d6f41eecc60ce57655a2ff1830ca391bdc03f
Bahrican Yesil <bahricanyesil@gmail.com>
2021-08-25 16:03:21 +0300

    AddNode function and its test are added

--- lib/core/alphabet_tree.dart
+++ lib/core/alphabet_tree.dart
@@ -1,5 +1,7 @@
 import 'dart:collection';
 
+import 'package:flutter/cupertino.dart';
+
 import 'node.dart';
 
 class AlphabetTree {
@@ -24,5 +26,34 @@ class AlphabetTree {
       elements.add(otherRoot.children[i].value);
       getElementsWithDFS(otherRoot.children[i], elements);
     }
+  }
+
+  void addNode({required Key parentKey, required Node newNode, Node? node}) {
+    node ??= root;
+    for (var i = 0; i < node.children.length; i++) {
+      if (node.children[i].uniqueKey == parentKey) {
+        node.children[i].children.add(newNode);
+      } else {
+        addNode(parentKey: parentKey, newNode: newNode, node: node.children[i]);
+      }
+    }
+  }
+
+  bool compareAll(AlphabetTree tree) {
+    if (tree.root.value != root.value) {
+      return false;
+    }
+    return compareDfs(tree.root, root);
+  }
+
+  bool compareDfs(Node node, Node otherNode) {
+    for (var i = 0; i < node.children.length; i++) {
+      if (node.children[i].value != otherNode.children[i].value) {
+        return false;
+      } else {
+        compareDfs(node.children[i], otherNode.children[i]);
+      }
+    }
+    return true;
   }
 }
--- test/alphabet_tree_test.dart
+++ test/alphabet_tree_test.dart
@@ -3,45 +3,68 @@ import 'package:bstevr_assessment/core/node.dart';
 import "package:flutter_test/flutter_test.dart";
 
 void main() {
-  test("Sets should be exactly the same with the given ones.", () {
-    const root1 = Node("Z", [
-      Node("T", [
-        Node("S", [Node("L", [])])
-      ]),
-      Node("I", [Node("Y", [])]),
-      Node("D", [Node("P", [])])
-    ]);
-    const tree1 = AlphabetTree(root1);
+  group('Alphabet Tree Tests', () {
+    test("Sets should be exactly the same with the given ones.", () {
+      final root1 = Node("Z", [
+        Node("T", [
+          Node("S", [Node("L", [])])
+        ]),
+        Node("I", [Node("Y", [])]),
+        Node("D", [Node("P", [])])
+      ]);
+      final tree1 = AlphabetTree(root1);
 
-    const root2 = Node("A", [
-      Node("D", [Node("P", [])]),
-      Node("D", [Node("P", [])]),
-      Node("L", [])
-    ]);
-    const tree2 = AlphabetTree(root2);
+      final root2 = Node("A", [
+        Node("D", [Node("P", [])]),
+        Node("D", [Node("P", [])]),
+        Node("L", [])
+      ]);
+      final tree2 = AlphabetTree(root2);
 
-    const root3 = Node("Z", [
-      Node("D", [Node("L", [])]),
-      Node("S", [Node("P", [])]),
-      Node("X", [])
-    ]);
-    const tree3 = AlphabetTree(root3);
+      final root3 = Node("Z", [
+        Node("D", [Node("L", [])]),
+        Node("S", [Node("P", [])]),
+        Node("X", [])
+      ]);
+      final tree3 = AlphabetTree(root3);
 
-    const root4 = Node("K", [
-      Node("T", [
-        Node("Q", [Node("M", [])])
-      ]),
-      Node("S", [Node("N", [])]),
-      Node("Z", [Node("B", [])])
-    ]);
-    const tree4 = AlphabetTree(root4);
+      final root4 = Node("K", [
+        Node("T", [
+          Node("Q", [Node("M", [])])
+        ]),
+        Node("S", [Node("N", [])]),
+        Node("Z", [Node("B", [])])
+      ]);
+      final tree4 = AlphabetTree(root4);
 
-    final set = tree1.compareTrees(tree2);
-    final set2 = tree1.compareTrees(tree3);
-    final set3 = tree1.compareTrees(tree4);
+      final set = tree1.compareTrees(tree2);
+      final set2 = tree1.compareTrees(tree3);
+      final set3 = tree1.compareTrees(tree4);
 
-    expect(set, {"A", "I", "S", "T", "Y", "Z"});
-    expect(set2, {"I", "T", "Y", "X"});
-    expect(set3, {"B", "D", "I", "K", "L", "M", "N", "P", "Q", "Y"});
+      expect(set, {"A", "I", "S", "T", "Y", "Z"});
+      expect(set2, {"I", "T", "Y", "X"});
+      expect(set3, {"B", "D", "I", "K", "L", "M", "N", "P", "Q", "Y"});
+    });
+
+    test("Node creation test", () {
+      final parentNode = Node("S", [Node("L", [])]);
+      final root1 = Node("Z", [
+        Node("T", [parentNode]),
+        Node("I", [Node("Y", [])]),
+        Node("D", [Node("P", [])])
+      ]);
+      final tree = AlphabetTree(root1);
+      final newNode = Node("G", []);
+      tree.addNode(parentKey: parentNode.uniqueKey, newNode: newNode);
+
+      final finalRoot = Node("Z", [
+        Node("T", [
+          Node("S", [Node("L", []), newNode])
+        ]),
+        Node("I", [Node("Y", [])]),
+        Node("D", [Node("P", [])])
+      ]);
+      expect(tree.compareAll(AlphabetTree(finalRoot)), true);
+    });
   });
 }
