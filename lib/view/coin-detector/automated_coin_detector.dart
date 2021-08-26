6d6d4d9f4d480f621efe446fd1437209f3eea011
Bahrican Yesil <bahricanyesil@gmail.com>
2021-08-26 11:00:30 +0300

    Coin detector page with animations

--- lib/view/coin-detector/automated_coin_detector.dart
+++ lib/view/coin-detector/automated_coin_detector.dart
@@ -64,48 +64,6 @@ class _CoinDetectorState extends State<CoinDetector> {
                   },
                 ),
               ),
-              // Container(
-              //   decoration: BoxDecoration(color: Colors.greenAccent),
-              //   child: Row(
-              //     mainAxisSize: MainAxisSize.max,
-              //     crossAxisAlignment: CrossAxisAlignment.center,
-              //     mainAxisAlignment: MainAxisAlignment.center,
-              //     children: <Widget>[
-              //       FlatButton(
-              //         onPressed: () {
-              //           setState(() {
-              //             _elementKey.currentState!.insertItem(0,
-              //                 duration: const Duration(milliseconds: 500));
-              //             _items = []
-              //               ..add(counter++)
-              //               ..addAll(_items);
-              //           });
-              //         },
-              //         child: Text(
-              //           "Add item to first",
-              //           style: TextStyle(color: Colors.black, fontSize: 20),
-              //         ),
-              //       ),
-              //       FlatButton(
-              //         onPressed: () {
-              //           if (_items.length <= 1) return;
-              //           _elementKey.currentState!.removeItem(
-              //               0,
-              //               (_, animation) =>
-              //                   slideAnimation(context, 0, animation),
-              //               duration: const Duration(milliseconds: 500));
-              //           setState(() {
-              //             _items.removeAt(0);
-              //           });
-              //         },
-              //         child: Text(
-              //           "Remove first item",
-              //           style: TextStyle(color: Colors.black, fontSize: 20),
-              //         ),
-              //       )
-              //     ],
-              //   ),
-              // ),
             ],
           ),
         );
